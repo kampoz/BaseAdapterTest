ec0cee49dd713d00021be828b757f3cfda11e6ae
Kamil Poznakowski <Kamil@Mac-mini-Kamil.local>
2017-08-10 18:36:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kamil/baseadaptertest/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.kamil.baseadaptertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+  GridView simpleGrid;
+  int animals[] = {
+      R.drawable.animal13,
+      R.drawable.animal14,
+      R.drawable.animal15,
+      R.drawable.animal16,
+      R.drawable.animal17,
+      R.drawable.animal18,
+      R.drawable.animal15,
+      R.drawable.animal16,
+      R.drawable.animal17
+  };
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+
+    simpleGrid = (GridView) findViewById(R.id.simpleGridView);
+    CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), animals);
+    simpleGrid.setAdapter(customAdapter);
+  }
+}
